04acfe1684e2b51663136e6d1ae5eac83e1b6c5f
ORDI merit <merit.seeeba@vikk.ee>
2025-07-17 19:46:19 +0300

    ok

--- AUTOD.py
+++ AUTOD.py
@@ -12,22 +12,28 @@ taust = pygame.image.load("bg_rally.jpg")
 punane_auto = pygame.image.load("f1_red.png")
 sinine_auto = pygame.image.load("f1_blue.png")
 
-# Punase auto asukoht
-punane_x = laius // 2 - punane_auto.get_width() // 2
+# Tee ala (vältimaks muru)
+tee_algus = 120
+tee_lõpp = laius - 120
+raja_laius = (tee_lõpp - tee_algus) // 3
+rajad = [tee_algus + raja_laius * i + (raja_laius - punane_auto.get_width()) // 2 for i in range(3)]
+
+# Punane auto algab keskmisel rajal
+punane_rada = 1
+punane_x = rajad[punane_rada]
 punane_y = kõrgus - punane_auto.get_height() - 10
 
-# Siniste autode algväärtused
+# Sinised autod (vältivad punase rada)
 sinine_autod = []
-for i in range(2):  # 3 sinist autot
-    x = random.randint(100, laius - 100)
-    y = random.randint(-350, -100)
-    sinine_autod.append([x, y])
+for _ in range(3):
+    rada = random.choice([i for i in range(3) if i != punane_rada])
+    x = rajad[rada]
+    y = random.randint(-480, -150)
+    sinine_autod.append([x, y, rada])
 
 # Font ja skoor
 font = pygame.font.SysFont(None, 36)
 skoor = 0
-
-# Kella taimer
 kell = pygame.time.Clock()
 kiirus = 4
 töötab = True
@@ -37,10 +43,17 @@ while töötab:
         if sündmus.type == pygame.QUIT:
             töötab = False
 
-    # Taust
-    ekraan.blit(taust, (0, 0))
+    # Liikumine rajal
+    klahvid = pygame.key.get_pressed()
+    if klahvid[pygame.K_LEFT] and punane_rada > 0:
+        punane_rada -= 1
+        punane_x = rajad[punane_rada]
+    if klahvid[pygame.K_RIGHT] and punane_rada < 2:
+        punane_rada += 1
+        punane_x = rajad[punane_rada]
 
-    # Punane auto
+    # Taust ja punane auto
+    ekraan.blit(taust, (0, 0))
     ekraan.blit(punane_auto, (punane_x, punane_y))
 
     # Sinised autod
@@ -48,15 +61,17 @@ while töötab:
         auto[1] += kiirus
         ekraan.blit(sinine_auto, (auto[0], auto[1]))
 
-        # Kui auto jõuab alla, liigub uuesti üles ja lisab skoori
+        # Kui jõuab alla, ilmub uuesti üles, vältides punase rada
         if auto[1] > kõrgus:
-            auto[1] = random.randint(-400, -100)
-            auto[0] = random.randint(100, laius - 100)
+            rada = random.choice([i for i in range(3) if i != punane_rada])
+            auto[0] = rajad[rada]
+            auto[1] = random.randint(-480, -50)
+            auto[2] = rada
             skoor += 1
 
-    # Skoori kuvamine
-    skoori_tekst = font.render("Skoor: " + str(skoor), True, (255, 255, 255))
-    ekraan.blit(skoori_tekst, (10, 10))
+    # Skoor
+    tekst = font.render(f"Skoor: {skoor}", True, (255, 255, 255))
+    ekraan.blit(tekst, (10, 10))
 
     pygame.display.flip()
     kell.tick(60)
